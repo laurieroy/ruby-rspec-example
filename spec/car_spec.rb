b7b5c53142b72a0e990bdb97c838118d6b6362cc
lauretteroy <laurieroy.dev@gmail.com>
2021-03-02 12:56:20 -0700

    Subject

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,31 +1,34 @@
 require 'car'
 
-describe 'Car' do
+describe Car do
 
 	describe 'attributes' do
-		let(:car) { Car.new }
+		# since have class name, do not need to explicitly define subject
+		# subject { Car.new }
+		# Use "subject" instead of 'let' if var is subj of example
+		# let(:car) { Car.new }
 		# "let" is better than before for setting up instance vars
 		# before(:example) do
 		# 	@car = Car.new
 		# end
 		it "allows reading and writing for :make" do
 			# skip("Debugging a problem")
-			car.make = 'Test'
-			expect(car.make).to eq('Test')
+			subject.make = 'Test'
+			expect(subject.make).to eq('Test')
 		end
 
 		it "allows reading and writing for :year" do
-			car.year = 9999
-			expect(car.year).to eq(9999)
+			subject.year = 9999
+			expect(subject.year).to eq(9999)
 		end
 
 		it "allows reading and writing for :color" do
-			car.color = 'foo'
-			expect(car.color).to eq('foo')
+			subject.color = 'foo'
+			expect(subject.color).to eq('foo')
 		end
 
 		it "Allows reading for :wheels" do
-			expect(car.wheels).to eq(4)
+			expect(subject.wheels).to eq(4)
 		end
 
 		it "allows writing for :doors"
@@ -43,7 +46,7 @@ describe 'Car' do
 	describe '#full_name' do
 		let (:honda) { Car.new(:make => 'Honda', :year => 2004, :color => 'blue') }
 		let(:new_car) { Car.new }
-		
+
 		it "returns a string in the expected format" do
 			expect(honda.full_name).to eq('2004 Honda (blue)')  
 		end
