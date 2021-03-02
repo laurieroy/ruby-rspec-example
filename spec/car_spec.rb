a8f2a0bee3a94100a4107c83e48514b5ecc14f6b
lauretteroy <laurieroy.dev@gmail.com>
2021-03-02 12:35:54 -0700

    Add before hook, let

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,28 +3,28 @@ require 'car'
 describe 'Car' do
 
 	describe 'attributes' do
+		let(:car) { Car.new }
+		# "let" is better than before for setting up instance vars
+		# before(:example) do
+		# 	@car = Car.new
+		# end
 		it "allows reading and writing for :make" do
 			# skip("Debugging a problem")
-			car = Car.new
 			car.make = 'Test'
 			expect(car.make).to eq('Test')
 		end
 
 		it "allows reading and writing for :year" do
-			car = Car.new
 			car.year = 9999
 			expect(car.year).to eq(9999)
 		end
 
 		it "allows reading and writing for :color" do
-			car = Car.new
 			car.color = 'foo'
 			expect(car.color).to eq('foo')
 		end
 
 		it "Allows reading for :wheels" do
-			car = Car.new
-
 			expect(car.wheels).to eq(4)
 		end
 
@@ -34,25 +34,23 @@ describe 'Car' do
 
 
 	describe '.colors' do
+		let(:colors) { [ 'blue', 'black', 'red', 'green'] }
 		it "returns an array of color names" do
-			c = [ 'blue', 'black', 'red', 'green']
-			
-			expect(Car.colors).to match_array(c)
+			expect(Car.colors).to match_array(colors)
 		end
 	end
 
 	describe '#full_name' do
+		let (:honda) { Car.new(:make => 'Honda', :year => 2004, :color => 'blue') }
+		let(:new_car) { Car.new }
+		
 		it "returns a string in the expected format" do
-			@honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
-
-			expect(@honda.full_name).to eq('2004 Honda (blue)')  
+			expect(honda.full_name).to eq('2004 Honda (blue)')  
 		end
 
 		context 'when initialized with no arguments' do
 			it "returns a string using default values" do
-				car = Car.new
-
-				expect(car.full_name).to eq('2007 Volvo (unknown)')
+				expect(new_car.full_name).to eq('2007 Volvo (unknown)')
 			end
 		end
 		
